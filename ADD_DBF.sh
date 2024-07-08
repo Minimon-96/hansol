67a7436b285c87e0851c9483be93cfb5ef4d4681
Minimon <98527303+Minimon-96@users.noreply.github.com>
2024-07-09 01:00:03 +0900

    Update and rename ADD_DBF to ADD_DBF.sh

--- ADD_DBF.sh
+++ ADD_DBF.sh
@@ -1,17 +1,26 @@
 #!/bin/sh
-USER_PATH="/inticube/smsom"
-#USER_PATH="/inticube/ipsmgwom"
+USER_PATH="/inticube/smsom/pilot/supportkt"
 
 ORACLE_DBF() {
-    export ORACLE_SID=HSMSCOM
-
-    SQLPLUS="/home/oracle/db/product/12.1.0.2/dbhome_1/bin/sqlplus"
+    ORACLE_SID="HSMSCOM"
     USERNAME="smsom"
     PASSWORD="smsom"
     TBS_NAME="OM_DATA"
     HOST="localhost"
     PORT="1521"
-    OUTPUT_FILE="$USER_PATH/pilot/supportkt/output.log"
+    SQLPLUS="/home/oracle/db/product/12.1.0.2/dbhome_1/bin/sqlplus"
+    OUTPUT_FILE="$USER_PATH/output.log"
+
+    ORA_CHECK=$($SQLPLUS $USERNAME/$PASSWORD@$ORACLE_SID << EOF
+    EXIT;
+EOF
+    )
+    if [[ $ORA_CHECK == *"Oracle"* ]]; then
+        echo "Oracle DB running."
+    else
+        echo "Oracle DB not running"
+        exit 1
+    fi
 
     $SQLPLUS -s /nolog <<EOF > /dev/null
 SPOOL $OUTPUT_FILE
@@ -24,7 +33,7 @@ SET LINESIZE 200
 SET TRIMSPOOL ON 
 col tablespace_name for a10 
 col file_name for a50
-SELECT tablespace_name, file_name, user_bytes/1024/1024 "USED(MB)", bytes/1024/1024 "total(MB)", maxbytes/1024/1024 "max(MB)", autoextensible "AUTO", online_status
+SELECT tablespace_name, file_name, user_bytes/1024/1024 "U\sed(MB)", bytes/1024/1024 "total(MB)", maxbytes/1024/1024 "max(MB)", autoextensible "AUTO", online_status
 FROM dba_data_files;
 SPOOL OFF
 EXIT;
@@ -38,7 +47,7 @@ EOF
     echo "Success: Query executed successfully"
     echo "Logging results to $OUTPUT_FILE"
 
-    current_file=$(awk -v tbs_name="$TBS_NAME" '$1 == tbs_name {print $2}' $OUTPUT_FILE|\tail -1)
+    current_file=$(\awk -v tbs_name="$TBS_NAME" '$1 == tbs_name {print $2}' $OUTPUT_FILE | \sort -k2,2n | \tail -1)
 
     if [ -z "$current_file" ]; then
         echo "Check the dbf file: $current_file"
@@ -46,7 +55,7 @@ EOF
     fi
 
     filename=$(basename "$current_file")
-    index=$(echo "$filename" | grep -o '[0-9]*' | tail -1)
+    index=$(echo "$filename" | \grep -o '[0-9]*' | \tail -1)
 
     if [ -z "$index" ]; then
         echo "Not Found Tablespace $TBS_NAME"
@@ -55,7 +64,7 @@ EOF
 
     next_index=$(printf "%02d" $((10#$index + 1)))
 
-    new_filename=$(echo "$filename" | sed "s/[0-9]*\./$next_index./")
+    new_filename=$(echo "$filename" | \sed "s/[0-9]*\./$next_index./")
     new_file_path=$(dirname "$current_file")/$new_filename
 
     echo -e "\n[Guide to adding dbf files]"
@@ -65,29 +74,41 @@ EOF
 }
 
 ALTIBASE_DBF() {
-    SQLPLUS="/altibase/altibase_home/bin/isql"
+    iSQL="/altibase/altibase_home/bin/isql"
     USERNAME="smsom"
     PASSWORD="smsom"
     TBS_NAME="OM_DATA"
     HOST="localhost"
     PORT="20300"
-    OUTPUT_FILE="$USER_PATH/pilot/supportkt/output.log"
-    isql='isql -s localhost  -u smsom -p smsom -port 20300'
-    $isql<<EOF > /dev/null
+    OUTPUT_FILE="$USER_PATH/output.log"
+    isql_cmd="$iSQL -s $HOST -u $USERNAME -p $PASSWORD -port $PORT"
+
+    ISQL_OUTPUT=$($isql_cmd << EOF
+    EXIT;
+EOF
+    )
+
+    if [[ $ISQL_OUTPUT == *"Altibase"* ]]; then
+        echo "Altibase DB running"
+    else
+        echo "Altibase DB not running"
+        exit 1
+    fi
+
+    $isql_cmd <<EOF > /dev/null
 set linesize 1024;
 set colsize 30;
 set feedback off;
 SPOOL $OUTPUT_FILE
-
 SELECT b.name tbs_name, a.id 'FILE#', a.name datafile_name,
              currsize*8/1024 'ALLOC(M)', round(case2(a.maxsize=0, currsize, a.maxsize)*8/1024) 'MAX(M)',
              decode(autoextend, 0, 'OFF', 'ON') 'AUTOEXTEND'
 FROM v\$datafiles a, v\$tablespaces b WHERE b.id = a.spaceid
 ORDER BY b.name, a.id;
-
 SPOOL OFF
 EXIT;
 EOF
+
     if [ $? -ne 0 ]; then
         echo "ISQL_CONNECTION ERROR"
         exit 1
@@ -96,7 +117,7 @@ EOF
     echo "Success: Query executed successfully"
     echo "Logging results to $OUTPUT_FILE"
 
-    current_file=$(awk -v tbs_name="$TBS_NAME" '$1 == tbs_name {print $3}' $OUTPUT_FILE |\tail -1)
+    current_file=$(\awk -v tbs_name="$TBS_NAME" '$1 == tbs_name {print $3}' $OUTPUT_FILE | \sort -k2,2n | \tail -1)
 
     if [ -z "$current_file" ]; then
         echo "Check the dbf file : $current_file"
@@ -104,7 +125,7 @@ EOF
     fi
 
     filename=$(basename "$current_file")
-    index=$(echo "$filename" | grep -o '[0-9]*' | tail -1)
+    index=$(echo "$filename" | \grep -o '[0-9]*' | \tail -1)
 
     if [ -z "$index" ]; then
         echo "Not Found Tablespace $TBS_NAME"
@@ -113,7 +134,7 @@ EOF
 
     next_index=$(printf "%02d" $((10#$index + 1)))
 
-    new_filename=$(echo "$filename" | sed "s/[0-9]*\./$next_index./")
+    new_filename=$(echo "$filename" | \sed "s/[0-9]*\./$next_index./")
     new_file_path=$(dirname "$current_file")/$new_filename
 
     echo -e "\n[Guide to adding dbf files]"
@@ -122,20 +143,30 @@ EOF
     echo "iSQL> ALTER TABLESPACE $TBS_NAME ADD DATAFILE '$new_file_path' AUTOEXTEND ON NEXT 100M MAXSIZE 31G;"
 }
 
-echo "=========================================="
-if [ -z "$USER_PATH/conf/OMDBMS" ]; then
-	echo "Not Found ~/conf/OMDBMS"
-	exit 1
-fi
+CHECK_DBMS() {
+    if ps -ef | \grep -q '[o]ra_pmon'; then
+        echo "oracle"
+        return
+    fi
 
-DBMS=`cat $USER_PATH/conf/OMDBMS`
+    if ps -ef | \grep -q '[a]ltibase'; then
+        echo "altibase"
+        return
+    fi
+
+    echo "unknown"
+}
+
+DBMS=$(CHECK_DBMS)
+
+echo "=========================================="
 echo "DB type : $DBMS"
 
-if [ $DBMS = 'oracle' ]; then
-	ORACLE_DBF
-elif [ $DBMS = 'altibase' ]; then
+if [ "$DBMS" = 'oracle' ]; then
+    ORACLE_DBF
+elif [ "$DBMS" = 'altibase' ]; then
     ALTIBASE_DBF
 else
-	echo "DB TYPE Error"
+    echo "DB TYPE Error"
 fi
 echo "=========================================="
